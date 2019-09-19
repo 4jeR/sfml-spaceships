ff1ab25de314f1b87498151787c687879cf59269
4jeR <bdlugosz98@gmail.com>
2019-09-19 09:23:04 +0200

    entry point for spaceships

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+
+
+
+int main(){
+
+    sf::RenderWindow window(sf::VideoMode(600, 800), "XD");
+
+    window.display();
+
+    return 0;
+}
